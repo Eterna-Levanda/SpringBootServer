2e6459dd999338e59a652eed3574823d0fe65e95
and.valente <and.valente@reply.it>
2025-11-21 18:16:13 +0100

    Actuator: ampliato l'output usando ApplicationContext

--- src/main/java/and/learn/actuator/MyInfoContributor.java
+++ src/main/java/and/learn/actuator/MyInfoContributor.java
@@ -1,8 +1,10 @@
 package and.learn.actuator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +20,34 @@ public class MyInfoContributor implements InfoContributor {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private ApplicationContext ctx;
+
+    //lettura dallo yml
+    @Value("${nome.ambiente}")
+    private String nomeAmbiente;
+
     @Override
     public void contribute(Info.Builder builder) {
+
         Map<String, Object> customInfo = new HashMap<>();
         customInfo.put("buildTimestamp", System.currentTimeMillis());
-        //qui leggo una prop dallo yml
-        customInfo.put("environment", environment.getProperty("nome.ambiente"));
-        customInfo.put("Sistema Operativo", System.getProperty("os.name"));
+        customInfo.put("sistema operativo", System.getProperty("os.name"));
+        //Lettura di property dallo yml in 2 modi diversi
+        customInfo.put("nomeAmbiente1", environment.getProperty("nome.ambiente"));
+        customInfo.put("nomeAmbiente2", nomeAmbiente);
+
+        Map<String, String> buildDetails = new HashMap<>();
+        buildDetails.put("package", getClass().getPackage().getName());
+
+        //Lettura di valori dall'ApplicationContext di Spring
+        Map<String, Object> contextDetails = new HashMap<>();
+        contextDetails.put("bean-definition-count", ctx.getBeanDefinitionCount());
+        contextDetails.put("startup-date", ctx.getStartupDate());
+
+        //struttura esterna del json info mostrato a video
         builder.withDetail("custom-info", customInfo);
+        builder.withDetail("context", contextDetails);
+        builder.withDetail("build", buildDetails);
     }
 }
